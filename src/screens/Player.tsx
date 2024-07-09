ee31fba7c3a59d936ae8b3697bc0a3373970529a
Alice <alice@ritsu.dev>
2024-07-09 21:30:15 +1000

    react-navigation

---
+++ src/screens/Player.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const Player = () => {
+  return (
+    <View>
+      <Text>Nyahaha</Text>
+    </View>
+  )
+}
+
+export default Player
---
+++ src/types/RootStackParamList.ts
@@ -0,0 +1,8 @@
+type RootStackParamList = {
+  Player: undefined
+  Settings: undefined
+  SelectServer: undefined
+  SelectUser: { server: string }
+}
+
+export default RootStackParamList
