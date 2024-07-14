9f471943aa7ba552bcb368f1bf76005405d0f2fa
Alice <alice@ritsu.dev>
2024-07-15 00:14:38 +1000

    add playback reporting

---
+++ src/lib/progress.ts
@@ -0,0 +1,53 @@
+import { sessions } from 'jellyfin-api'
+import ProgressQuery, {
+  ProgressStoppedQuery,
+} from 'jellyfin-api/lib/types/queries/ProgressQuery'
+import TrackPlayer, { State } from 'react-native-track-player'
+
+import useClient from 'hooks/useClient'
+import usePlayer from 'hooks/usePlayer'
+import secsToTicks from './secsToTicks'
+
+export const playing = async (
+  event: 'timeupdate' | 'pause' | 'unpause' | undefined,
+  itemID?: string,
+) => {
+  const position = (await TrackPlayer.getProgress()).position
+  const state = (await TrackPlayer.getPlaybackState()).state
+  const payload: ProgressQuery = {
+    CanSeek: true,
+    ItemId: itemID ?? usePlayer.getState().trackID,
+    MediaSourceId: itemID ?? usePlayer.getState().trackID,
+    EventName: event,
+    IsPaused: state === State.Paused,
+    IsMuted: false,
+    VolumeLevel: 100,
+    PositionTicks: secsToTicks(position),
+    PlayMethod: 'Transcode',
+    RepeatMode:
+      usePlayer.getState().repeat === 'off'
+        ? 'RepeatNone'
+        : usePlayer.getState().repeat === 'queue'
+        ? 'RepeatAll'
+        : 'RepeatOne',
+  }
+  if (event === undefined) {
+    await sessions.playing(useClient.getState().api, payload)
+  } else {
+    await sessions.playingProgress(useClient.getState().api, payload)
+  }
+}
+
+export const stopped = async (
+  itemID?: string,
+  position?: number,
+  failed: boolean = false,
+) => {
+  const payload: ProgressStoppedQuery = {
+    ItemId: itemID ?? usePlayer.getState().trackID,
+    MediaSourceId: itemID ?? usePlayer.getState().trackID,
+    PositionTicks: secsToTicks(position),
+    Failed: failed,
+  }
+  await sessions.playingStopped(useClient.getState().api, payload)
+}
--- src/services/PlaybackService.ts
+++ src/services/PlaybackService.ts
@@ -1,5 +1,6 @@
 import usePlayer from 'hooks/usePlayer'
-import TrackPlayer, { Event } from 'react-native-track-player'
+import { playing, stopped } from 'lib/progress'
+import TrackPlayer, { Event, State } from 'react-native-track-player'
 
 const PlaybackService = async () => {
   TrackPlayer.addEventListener(Event.RemotePlay, () =>
@@ -14,23 +15,43 @@ const PlaybackService = async () => {
   // )
   TrackPlayer.addEventListener(Event.PlayerError, (error) => console.log(error))
 
-  TrackPlayer.addEventListener(Event.PlaybackActiveTrackChanged, (event) => {
-    if (usePlayer.getState().queue.length > event.index) {
-      usePlayer.getState().setTrack(event.index, true)
-      if ('NormalizationGain' in usePlayer.getState().queue[event.index]) {
-        const gain = Math.pow(
-          10,
-          usePlayer.getState().queue[event.index].NormalizationGain / 20,
-        )
-        TrackPlayer.setVolume(gain)
-      } else {
-        TrackPlayer.setVolume(1.0)
+  TrackPlayer.addEventListener(
+    Event.PlaybackActiveTrackChanged,
+    async (event) => {
+      if (usePlayer.getState().queue.length > event.index) {
+        usePlayer.getState().setTrack(event.index, true)
+        if ('NormalizationGain' in usePlayer.getState().queue[event.index]) {
+          const gain = Math.pow(
+            10,
+            usePlayer.getState().queue[event.index].NormalizationGain / 20,
+          )
+          await TrackPlayer.setVolume(gain)
+        } else {
+          await TrackPlayer.setVolume(1.0)
+        }
       }
-    }
+
+      if ('track' in event && event.lastTrack) {
+        const lastID = usePlayer.getState().queue[event.lastIndex].Id
+        await stopped(lastID, event.lastTrack.duration)
+      }
+      const trackID = usePlayer.getState().queue[event.index].Id
+      await playing(undefined, trackID)
+    },
+  )
+
+  TrackPlayer.addEventListener(Event.PlaybackProgressUpdated, async (event) => {
+    await playing('timeupdate')
   })
 
-  TrackPlayer.addEventListener(Event.PlaybackProgressUpdated, (event) => {
-    // TODO)) playback reporting
+  TrackPlayer.addEventListener(Event.PlaybackState, async (event) => {
+    if (event.state === State.Paused) {
+      await playing('pause')
+    } else if (event.state === State.Playing) {
+      await playing('unpause')
+    } else if (event.state === State.Stopped) {
+      await stopped()
+    }
   })
 }
 
